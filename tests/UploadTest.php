ce2160319fdd58cc25702f470af38efa47db6060
Tony NGUEREZA <nguerezatony@gmail.com>
2021-04-03 18:19:26 +0100

    initial commit

---
+++ tests/UploadTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Platine\Test\Upload;
+
+/**
+ * Upload class tests
+ *
+ * @group core
+ * @group upload
+ */
+class UploadTest extends PlatineTestCase
+{
+
+    public function testConstructor()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/fixtures/mocks.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Platine\Upload;
+
+$mock_demo_to_false = false;
